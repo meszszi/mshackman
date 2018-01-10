5f288ab2cd5a797674aa523a58b1d8ca7d97db19
Mateusz Szpyrka <mjsz1701@gmail.com>
2018-01-10 23:17:41 +0100

    Added map field to all MapElement objects. Resigned from ISolid interface, added accessible flag to MapField instead. Added BugType enum. Other minor changes.

---
+++ src/main/java/pl/meszszi/mshackman/field/MapField.java
@@ -0,0 +1,51 @@
+package main.java.pl.meszszi.mshackman.field;
+
+import main.java.pl.meszszi.mshackman.MapElement;
+import main.java.pl.meszszi.mshackman.MoveDirection;
+import main.java.pl.meszszi.mshackman.Position;
+import main.java.pl.meszszi.mshackman.maps.GameMap;
+
+import java.util.ArrayList;
+
+/**
+ * Class used for representing accessibility properties of a field.
+ */
+
+public class MapField extends MapElement{
+
+    private final boolean accessible;
+
+    public MapField(GameMap map, Position position, boolean accessible) {
+        super(map, position);
+        this.accessible = accessible;
+    }
+
+
+    /**
+     * Checks if the field can be accessed by any character (player or bug).
+     * @return this.accessible
+     */
+    public boolean isAccessible() {
+        return this.accessible;
+    }
+
+
+    /**
+     * Gets all fields that can be accessed directly from this (assumes that this one is by definition accessible).
+     * @return list of adjacent accessible MapFields.
+     */
+    public ArrayList<MapField> getAdjacentFields() {
+        ArrayList<MapField> adjacent = new ArrayList<>();
+
+        for(MoveDirection direction : MoveDirection.values()) {
+            Position adjPosition = this.position.move(direction);
+            MapField field = this.map.getField(adjPosition);
+
+            if(field == null || !field.isAccessible())  // Skips the loop iteration if the field is outside the map or is inaccessible.
+                continue;
+
+        }
+
+        return adjacent;
+    }
+}
